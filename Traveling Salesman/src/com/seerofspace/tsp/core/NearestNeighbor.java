a470a6ee1188a52ffed5a7b17d3445c7cc7d8a29
SeerOfSpace <asdf>
2019-10-07 07:05:08 -0400

    a

--- Traveling Salesman/src/com/seerofspace/tsp/core/NearestNeighbor.java
+++ Traveling Salesman/src/com/seerofspace/tsp/core/NearestNeighbor.java
@@ -3,8 +3,10 @@ package com.seerofspace.tsp.core;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Iterator;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
+import java.util.Queue;
 
 import com.seerofspace.tsp.graph.Edge;
 import com.seerofspace.tsp.graph.Graph;
@@ -32,6 +34,8 @@ public class NearestNeighbor {
 			if(isOnlyCircular(currentNode)) {
 				throw new RuntimeException("All edges are circular");
 			}
+			
+			/*
 			Iterator<Edge<IdType, WeightType>> iterator = currentNode.getAdjacentIterator();
 			Edge<IdType, WeightType> lowest = null;
 			Edge<IdType, WeightType> lowestVisited = null;
@@ -65,6 +69,7 @@ public class NearestNeighbor {
 			route.add(result);
 			visitedNodes.putIfAbsent(result.getId(), result);
 			currentNode = result;
+			*/
 		}
 		
 		route.add(startingNode);
@@ -80,6 +85,106 @@ public class NearestNeighbor {
 			}
 		}
 		return true;
+	}
+	
+	public static <IdType, WeightType extends Comparable<WeightType>> Node<IdType, WeightType> findNextPathShallow(
+			Node<IdType, WeightType> currentNode, 
+			Map<IdType, Node<IdType, WeightType>> visitedNodes) {
+		
+		Iterator<Edge<IdType, WeightType>> iterator = currentNode.getAdjacentIterator();
+		Edge<IdType, WeightType> lowest = null;
+		while(iterator.hasNext()) {
+			Edge<IdType, WeightType> temp = iterator.next();
+			if(temp.getDestination() == currentNode) {
+				continue;
+			}
+			if(visitedNodes.containsKey(temp.getDestination().getId())) {
+				if(lowest == null) {
+					lowest = temp;
+				} else if(temp.getWeight().compareTo(lowest.getWeight()) < 0) {
+					lowest = temp;
+				}
+			}
+		}
+		return lowest.getDestination();
+	}
+	
+	private static class BacktraceNode<IdType, WeightType> {
+		
+		private Node<IdType, WeightType> node;
+		private BacktraceNode<IdType, WeightType> parent;
+		
+		public BacktraceNode(Node<IdType, WeightType> node, BacktraceNode<IdType, WeightType> parent) {
+			this.node = node;
+			this.parent = parent;
+		}
+		
+		public Node<IdType, WeightType> getNode() {
+			return node;
+		}
+		
+		public BacktraceNode<IdType, WeightType> getParent() {
+			return parent;
+		}
+		
+	}
+	
+	public static <IdType, WeightType extends Comparable<WeightType>> List<Node<IdType, WeightType>> findNextPathDeep(
+			Node<IdType, WeightType> currentNode, 
+			Map<IdType, Node<IdType, WeightType>> visitedNodes) {
+		
+		Queue<BacktraceNode<IdType, WeightType>> queue = new LinkedList<>();
+		List<Node<IdType, WeightType>> list = getOrderedAdjacentList(currentNode);
+		list.forEach(e -> {
+			queue.add(new BacktraceNode<IdType, WeightType>(e, null));
+		});
+		
+		Map<IdType, Node<IdType, WeightType>> visitedNodesQueue = new HashMap<>();
+		visitedNodesQueue.put(currentNode.getId(), currentNode);
+		
+		while(!queue.isEmpty()) {
+			
+			BacktraceNode<IdType, WeightType> nextNode = queue.poll();
+			visitedNodesQueue.putIfAbsent(nextNode.getNode().getId(), nextNode.getNode());
+			
+			if(!visitedNodes.containsKey(nextNode.getNode().getId())) {
+				List<Node<IdType, WeightType>> path = new ArrayList<>();
+				do {
+					path.add(nextNode.getNode());
+					nextNode = nextNode.getParent();
+				} while(nextNode != null);
+				return path;
+			}
+			
+			if(nextNode.getNode().getAdjacentSize() != 0) {
+				list = getOrderedAdjacentList(nextNode.getNode());
+				for(Node<IdType, WeightType> e : list) {
+					if(!visitedNodesQueue.containsKey(e.getId())) {
+						queue.add(new BacktraceNode<IdType, WeightType>(e, nextNode));
+					}
+				}
+			}
+			
+		}
+		
+		return null;
+	}
+	
+	private static <IdType, WeightType extends Comparable<WeightType>> List<Node<IdType, WeightType>> getOrderedAdjacentList(
+			Node<IdType, WeightType> node) {
+		
+		List<Edge<IdType, WeightType>> list = new ArrayList<>(node.getAdjacentSize());
+		List<Node<IdType, WeightType>> result = new ArrayList<>(node.getAdjacentSize());
+		node.getAdjacentIterator().forEachRemaining(e -> {
+			list.add(e);
+		});
+		list.sort((e1, e2) -> {
+			return e1.getWeight().compareTo(e2.getWeight());
+		});
+		list.forEach(e -> {
+			result.add(e.getDestination());
+		});
+		return result;
 	}
 	
 }
